a8172c4e5119300d6644dc4eb24e7e0fcc91a232
Sachin <sachingoy@gmail.com>
2022-07-25 15:44:25 +0530

    Initial commit

---
+++ src/main/java/my/practice/code/App.java
@@ -0,0 +1,13 @@
+package my.practice.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! SachinGOYAL" );
+    }
+}
